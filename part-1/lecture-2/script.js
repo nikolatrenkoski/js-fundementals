f07253be994de37f4ec1c44ee81bdb252b1bc4f6
nikolatrenkoski <slutatjata1337@gmail.com>
2021-11-25 22:09:46 +0100

    move file to the right directory

---
+++ part-1/lecture-2/script.js
@@ -0,0 +1,11 @@
+let country = "Sweden";
+let continent = "Europe";
+let population = 10000000;
+let isIsland = false;
+let language;
+
+console.log(typeof country)
+console.log(typeof continent)
+console.log(typeof population)
+console.log(typeof isIsland)
+console.log(typeof language)
