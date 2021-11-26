a342010d12b6f6c97f42e3474e1e87d30711e549
nikolatrenkoski <slutatjata1337@gmail.com>
2021-11-26 19:15:50 +0100

    add lecture-3

---
+++ part-1/lecture-3/scripts.js
@@ -0,0 +1,17 @@
+let country = "Sweden";
+let continent = "Europe";
+let population = 10;
+let isIsland = false;
+let language = "Swedish";
+
+
+let halfPopulation = population / 2;
+let increasePopulation = population + 1;
+let biggerPopulationThenFinalnd = population > 6;
+let averagePopulation = population > 33;
+let desciption = `${country} is in ${continent}, and its ${population} million people speak ${language}`
+
+
+console.log(halfPopulation)
+console.log(increasePopulation)
+console.log(desciption)
