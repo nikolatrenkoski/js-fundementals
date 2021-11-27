e9f6b93ad95b919dab8addae6a6287a3e664206e
nikolatrenkoski <slutatjata1337@gmail.com>
2021-11-27 22:04:01 +0100

    add lecture-5

---
+++ assignment/lecture-5/script.js
@@ -0,0 +1,14 @@
+let country = "Sweden";
+let continent = "Europe";
+let population = 10;
+let isIsland = false;
+let language = "Swedish";
+
+
+if (population > 33) {
+    console.log(`${country}s population is above average`)
+}
+
+else {
+    console.log(`${country}s population is ${33 - population} million below average`)
+}
