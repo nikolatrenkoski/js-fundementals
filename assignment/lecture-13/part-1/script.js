52884211cc88a68829f1fe3785fd9cafbcb5ddb5
nikolatrenkoski <slutatjata1337@gmail.com>
2021-11-28 23:27:01 +0100

    add lecture-13 part-1

---
+++ assignment/lecture-13/part-1/script.js
@@ -0,0 +1,22 @@
+const day = "friday";
+
+if (day === "monday") {
+    console.log("plan course structure");
+    console.log("go to coding meetup");
+}
+
+else if (day === "thuesday") {
+    console.log("prepare theory videos");
+}
+
+else if (day === "wednesday" || day === "thursday") {
+    console.log("write code examples")
+}
+
+else if (day === "friday") {
+    console.log("record videos");
+}
+
+else if (day == "saturday" || day === "sunday") {
+    console.log("enjoy the weekend :D");
+}
--- code-challenge/fundementals-1/part-3/script.js
+++ code-challenge/fundementals-1/part-3/script.js
@@ -8,11 +8,7 @@ const koalaScoreTwo = 95;
 const koalaScoreThree = 106;
 const koalaAverageScore = (koalaScoreOne + koalaScoreTwo + koalaScoreThree) / 3;
 
-
-if (dolphinAverageScore > koalaAverageScore && dolphinAverageScore >= 100) console.log("Dolphins win!")
-
-else if (koalaAverageScore > dolphinAverageScore && koalaAverageScore >= 100) console.log("Koalas win!")
-
-else if (dolphinAverageScore === koalaAverageScore && koalaAverageScore >= 100) console.log("draw")
-
-else console.log("No team wins, score to low!")
+if (dolphinAverageScore > koalaAverageScore && dolphinAverageScore >= 100) console.log("Dolphins win!");
+else if (koalaAverageScore > dolphinAverageScore && koalaAverageScore >= 100) console.log("Koalas win!");
+else if (dolphinAverageScore === koalaAverageScore && koalaAverageScore >= 100) console.log("draw");
+else console.log("No team wins, score to low!");
