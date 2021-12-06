a11ae5debbbaa7cbd08dc81efd6614e37cb48fec
nikolatrenkoski <slutatjata1337@gmail.com>
2021-12-06 23:58:38 +0100

    add lecture-21, its not done yet ;)

---
+++ assignment/part-2/lecture-21/script.js
@@ -0,0 +1,15 @@
+function describePopulation(country, population) {
+    console.log(`${country} has ${population} million people, so its about ${percentageOfWorld1}% of the world`);
+    return describePopulation;
+}
+
+function percentageOfWorld1(percent) {
+    const percentage = population / 7900 * 100
+    return percentageOfWorld1;
+}
+
+const china = describePopulation("china", 1440)
+const sweden = describePopulation("sweden", 10)
+const netherlands = describePopulation("netherlands", 17)
+
+console.log(china, sweden, netherlands)
