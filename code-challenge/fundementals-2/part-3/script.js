1ba3c1c0d0e20c8423fa918aeb56d8606410518d
nikolatrenkoski <slutatjata1337@gmail.com>
2021-12-16 00:28:54 +0100

    add features to part-3 in code-challenge

--- code-challenge/fundementals-2/part-3/script.js
+++ code-challenge/fundementals-2/part-3/script.js
@@ -1,14 +1,30 @@
-
 const mark = {
     firstName: ["Mark"],
     mass: [78],
     height: [1.69],
 
     calcBMI: function (mass, height) {
-        const bmi = mass / height ** 2
-        console.log(bmi)
-        return bmi
+        this.bmi = mass / height ** 2;
+        console.log(this.bmi);
+        return this.bmi;
     }
+
 }
 
-mark.calcBMI(mark.mass, mark.height)
+const john = {
+    firstName: ["John"],
+    mass: [92],
+    height: [1.95],
+
+    calcBMI: function (mass, height) {
+        this.bmi = mass / height ** 2;
+        console.log(this.bmi);
+        return this.bmi;
+    },
+}
+
+
+//i know that im reapting code, but the the coding-challing explicity said to write calcBMI method on each object..
+
+
+john.calcBMI(john.mass, john.height) >= mark.calcBMI(mark.mass, mark.height) ? console.log(`John's BMI ${john.bmi} is higher then Marks's ${mark.bmi}`) : console.log(`Mark's BMI ${mark.bmi} is higher then John's ${john.bmi}`)
